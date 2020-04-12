ecc23496f32fcc40397a15faf4e0623e60845aea
Ark74 <ark@switnet.org>
2020-04-12 11:13:25 -0500

    Improve brandless mode

--- jm-bm.sh
+++ jm-bm.sh
@@ -7,13 +7,16 @@
 CSS_FILE="/usr/share/jitsi-meet/css/all.css"
 TITLE_FILE="/usr/share/jitsi-meet/title.html"
 INT_CONF="/usr/share/jitsi-meet/interface_config.js"
+BUNDLE_JS="/usr/share/jitsi-meet/libs/app.bundle.min.js"
 #
 JM_IMG_PATH="/usr/share/jitsi-meet/images/"
 WTM2_PATH="$JM_IMG_PATH/watermark2.png"
 FICON_PATH="$JM_IMG_PATH/favicon2.ico"
 #
 APP_NAME="Conferences"
+MOVILE_APP_NAME="Jitsi Meet"
 PART_USER="Participant"
+LOCAL_USER="me"
 echo '
 #--------------------------------------------------
 # Applying Brandless mode
@@ -36,6 +39,7 @@ fi
 sed -i "s|watermark.png|watermark2.png|g" $CSS_FILE
 sed -i "s|favicon.ico|favicon2.ico|g" $TITLE_FILE
 sed -i "s|jitsilogo.png|watermark2.png|g" $TITLE_FILE
+sed -i "s|logo-deep-linking.png|watermark2.png|g" $BUNDLE_JS
 
 #Disable logo and url
 sed -i "s|.leftwatermark{|.leftwatermark{display:none;|" $CSS_FILE
@@ -44,11 +48,13 @@ sed -i "s|.leftwatermark{|.leftwatermark{display:none;|" $CSS_FILE
 sed -i "s|Jitsi Meet|$APP_NAME|g" $TITLE_FILE
 sed -i "s| powered by the Jitsi Videobridge||g" $TITLE_FILE
 sed -i "21,32 s|Jitsi Meet|$APP_NAME|g" $INT_CONF
+sed -i "/appNotInstalled/ s|{{app}}|$MOVILE_APP_NAME|" /usr/share/jitsi-meet/lang/*
 
 #Custom UI changes
 echo "
 Please note that brandless mode will also overwrite support links.
 "
 sed -i "s|Fellow Jitster|$PART_USER|g" $INT_CONF
+sed -i "s|'me'|'$LOCAL_USER'|" $INT_CONF
 sed -i "s|LIVE_STREAMING_HELP_LINK: .*|LIVE_STREAMING_HELP_LINK: '#',|g" $INT_CONF
 sed -i "s|SUPPORT_URL: .*|SUPPORT_URL: '#',|g" $INT_CONF
