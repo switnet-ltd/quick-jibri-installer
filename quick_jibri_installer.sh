92c7063bd26bde8297d832e1837e8cc7d5dd8172
Ark74 <ark@switnet.org>
2020-04-13 13:39:25 -0500

    Remove customized send log

--- quick_jibri_installer.sh
+++ quick_jibri_installer.sh
@@ -397,7 +397,7 @@ systemctl stop $1
 	sed -i "s|/etc/jitsi/meet/$3.key|/etc/letsencrypt/live/$3/privkey.pem|" $4
 systemctl restart $1
 	#Add cron
-	crontab -l | { cat; echo "@weekly certbot renew --${2} > $LE_RENEW_LOG 2>&1 || mail -s 'LE SSL Errors' $SYSADMIN_EMAIL < $LE_RENEW_LOG"; } | crontab -
+	crontab -l | { cat; echo "@weekly certbot renew --${2} > $LE_RENEW_LOG 2>&1"; } | crontab -
 	crontab -l
 }
 
