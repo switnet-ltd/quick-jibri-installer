175f38616bb1c5327b39debe0b3aa04b69528d32
Ark74 <ark@switnet.org>
2021-07-05 12:14:32 -0500

    Fix comments configuration

--- jra_nextcloud.sh
+++ jra_nextcloud.sh
@@ -301,10 +301,10 @@ server {
     gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
 
     # Pagespeed is not supported by Nextcloud, so if your server is built
-    # with the `ngx_pagespeed` module, uncomment this line to disable it.
+    # with the \`ngx_pagespeed\` module, uncomment this line to disable it.
     #pagespeed off;
 
-    # HTTP response headers borrowed from Nextcloud `.htaccess`
+    # HTTP response headers borrowed from Nextcloud \`.htaccess\`
     add_header Referrer-Policy                      "no-referrer"   always;
     add_header X-Content-Type-Options               "nosniff"       always;
     add_header X-Download-Options                   "noopen"        always;
@@ -323,19 +323,19 @@ server {
     # Path to the root of your installation
     root $NC_PATH/;
 
-    # Specify how to handle directories -- specifying `/index.php\$request_uri`
+    # Specify how to handle directories -- specifying \`/index.php\$request_uri\`
     # here as the fallback means that Nginx always exhibits the desired behaviour
     # when a client requests a path that corresponds to a directory that exists
     # on the server. In particular, if that directory contains an index.php file,
     # that file is correctly served; if it doesn't, then the request is passed to
     # the front-end controller. This consistent behaviour means that we don't need
     # to specify custom rules for certain paths (e.g. images and other assets,
-    # `/updater`, `/ocm-provider`, `/ocs-provider`), and thus
-    # `try_files \$uri \$uri/ /index.php\$request_uri`
+    # \`/updater\`, \`/ocm-provider\`, \`/ocs-provider\`), and thus
+    # \`try_files \$uri \$uri/ /index.php\$request_uri\`
     # always provides the desired behaviour.
     index index.php index.html /index.php\$request_uri;
 
-    # Rule borrowed from `.htaccess` to handle Microsoft DAV clients
+    # Rule borrowed from \`.htaccess\` to handle Microsoft DAV clients
     location = / {
         if ( \$http_user_agent ~ ^DavClnt ) {
             return 302 /remote.php/webdav/\$is_args\$args;
@@ -348,13 +348,13 @@ server {
         access_log off;
     }
 
-    # Make a regex exception for `/.well-known` so that clients can still
+    # Make a regex exception for \`/.well-known\` so that clients can still
     # access it despite the existence of the regex rule
-    # `location ~ /(\.|autotest|...)` which would otherwise handle requests
-    # for `/.well-known`.
+    # \`location ~ /(\.|autotest|...)\` which would otherwise handle requests
+    # for \`/.well-known\`.
     location ^~ /.well-known {
         # The rules in this block are an adaptation of the rules
-        # in `.htaccess` that concern `/.well-known`.
+        # in \`.htaccess\` that concern \`/.well-known\`.
 
         location = /.well-known/carddav { return 301 /remote.php/dav/; }
         location = /.well-known/caldav  { return 301 /remote.php/dav/; }
@@ -362,18 +362,18 @@ server {
         location /.well-known/acme-challenge    { try_files \$uri \$uri/ =404; }
         location /.well-known/pki-validation    { try_files \$uri \$uri/ =404; }
 
-        # Let Nextcloud's API for `/.well-known` URIs handle all other
+        # Let Nextcloud's API for \`/.well-known\` URIs handle all other
         # requests by passing them to the front-end controller.
         return 301 /index.php\$request_uri;
     }
 
-    # Rules borrowed from `.htaccess` to hide certain paths from clients
+    # Rules borrowed from \`.htaccess\` to hide certain paths from clients
     location ~ ^/(?:build|tests|config|lib|3rdparty|templates|data)(?:\$|/)  { return 404; }
     location ~ ^/(?:\.|autotest|occ|issue|indie|db_|console)                { return 404; }
 
     # Ensure this block, which passes PHP files to the PHP process, is above the blocks
     # which handle static assets (as seen below). If this block is not declared first,
-    # then Nginx will encounter an infinite rewriting loop when it prepends `/index.php`
+    # then Nginx will encounter an infinite rewriting loop when it prepends \`/index.php\`
     # to the URI, resulting in a HTTP 500 error response.
     location ~ \.php(?:\$|/) {
         fastcgi_split_path_info ^(.+?\.php)(/.*)\$;
@@ -396,17 +396,17 @@ server {
 
     location ~ \.(?:css|js|svg|gif)\$ {
         try_files \$uri /index.php\$request_uri;
-        expires 6M;         # Cache-Control policy borrowed from `.htaccess`
+        expires 6M;         # Cache-Control policy borrowed from \`.htaccess\`
         access_log off;     # Optional: Don't log access to assets
     }
 
     location ~ \.woff2?\$ {
         try_files \$uri /index.php\$request_uri;
-        expires 7d;         # Cache-Control policy borrowed from `.htaccess`
+        expires 7d;         # Cache-Control policy borrowed from \`.htaccess\`
         access_log off;     # Optional: Don't log access to assets
     }
 
-    # Rule borrowed from `.htaccess`
+    # Rule borrowed from \`.htaccess\`
     location /remote {
         return 301 /remote.php\$request_uri;
     }
